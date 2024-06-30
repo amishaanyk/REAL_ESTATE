37f209ed82526f1d95522d574b2b0ef698882774
amishaanyk <amishanyk02@gmail.com>
2024-07-01 00:53:39 +0530

    FINAL CODE

---
+++ client/src/utils/accordion.jsx
@@ -0,0 +1,23 @@
+import { HiShieldCheck } from "react-icons/hi";
+import { MdCancel, MdAnalytics } from "react-icons/md";
+const data = [
+  {
+    icon: <HiShieldCheck/>,
+    heading: "Best interest rates on the market",
+    detail:
+      "We pride ourselves on offering the most competitive interest rates available. Our financial experts work tirelessly to secure rates that are tailored to your budget, ensuring that your investment is both affordable and profitable.",
+  },
+  {
+    icon: <MdCancel />,
+    heading: "Prevent unstable prices",
+    detail:
+      "Stability in pricing is a cornerstone of our service. We constantly monitor market trends to protect your investment from unexpected fluctuations, providing you with peace of mind and consistent value.",
+  },
+  {
+    icon: <MdAnalytics />,
+    heading: "Best price on the market",
+    detail:
+      "Our commitment to affordability means that you’ll always get the best price for your real estate needs. We strive to offer unbeatable prices without compromising on quality, making your dream home or investment a reality.",
+  },
+];
+export default data;
---
+++ server/config/auth0Config.js
@@ -0,0 +1,9 @@
+import {auth} from 'express-oauth2-jwt-bearer'
+
+const jwtCheck = auth({
+    audience: "http://localhost:8000",
+    issuerBaseURL: "https://dev-4u25na4zxyvhfq1z.us.auth0.com      ",
+    tokenSigningAlg: "RS256"
+})
+
+export default jwtCheck
