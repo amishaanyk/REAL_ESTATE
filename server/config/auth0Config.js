a037b55267e86f9ac1dd3103f7f75a56ef090231
amishaanyk <amishanyk02@gmail.com>
2024-07-01 02:42:21 +0530

    Update auth0Config.js

--- server/config/auth0Config.js
+++ server/config/auth0Config.js
@@ -1,7 +1,7 @@
 import {auth} from 'express-oauth2-jwt-bearer'
 
 const jwtCheck = auth({
-    audience: "http://localhost:8000",
+    audience: "https://dev-4u25na4zxyvhfq1z.us.auth0.com/api/v2/",
     issuerBaseURL: "https://dev-4u25na4zxyvhfq1z.us.auth0.com",
     tokenSigningAlg: "RS256"
 })
